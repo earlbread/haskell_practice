20d6726257872703bfd4b7f892ca8d47998f1f19
Seunghun Lee <waydi1@gmail.com>
2017-01-14 03:07:19 -0800

    Update wc.hs
    Update to print characters and words count.

--- wc.hs
+++ wc.hs
@@ -1,4 +1,4 @@
 -- wc.hs
--- Counts lines of input
+-- Counts chracters, words and lines of input
 main = interact wordCount
-    where wordCount input = (show (length (lines input)) ++ "\n")
+    where wordCount input = ((show (length (input))) ++ "\n" ++ (show (length (words input))) ++ "\n" ++  (show (length (lines input))) ++ "\n")
